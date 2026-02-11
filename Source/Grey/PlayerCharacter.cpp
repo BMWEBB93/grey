4a933c8ed03c81b809fb00aae5f1fc8e6f21a4c1
BMWebb03 <bmw129@student.aru.ac.uk>
2026-02-11 18:10:10 +0000

    building player character

--- Source/Grey/PlayerCharacter.cpp
+++ Source/Grey/PlayerCharacter.cpp
@@ -2,4 +2,76 @@
 
 
 #include "PlayerCharacter.h"
+#include "BaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+APlayerCharacter::APlayerCharacter()
+{
+	// Tick every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+    // Camera boom
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 300.f;
+    CameraBoom->bUsePawnControlRotation = true;
+
+    // Camera
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom);
+    FollowCamera->bUsePawnControlRotation = false;
+
+}
+
+void APlayerCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::Turn);
+    PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::LookUp);
+}
+
+void APlayerCharacter::MoveForward(float Value)
+{
+    if (Controller && Value != 0.f)
+    {
+        const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+        AddMovementInput(Direction, Value);
+    }
+}
+
+void APlayerCharacter::MoveRight(float Value)
+{
+    if (Controller && Value != 0.f)
+    {
+        const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+        AddMovementInput(Direction, Value);
+    }
+}
+
+void APlayerCharacter::Turn(float Value)
+{
+    AddControllerYawInput(Value);
+}
+
+void APlayerCharacter::LookUp(float Value)
+{
+    AddControllerPitchInput(Value);
+}
--- Source/Grey/PlayerCharacter.h
+++ Source/Grey/PlayerCharacter.h
@@ -14,4 +14,29 @@ class GREY_API APlayerCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
 	
+
+public:
+	APlayerCharacter();
+
+protected:
+    virtual void BeginPlay() override;
+
+    // Input handlers
+    void MoveForward(float Value);
+    void MoveRight(float Value);
+    void Turn(float Value);
+    void LookUp(float Value);
+
+
+	// Camera boom (spring arm)
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+    class USpringArmComponent* CameraBoom;
+
+    // Follow camera
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+    class UCameraComponent* FollowCamera;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
